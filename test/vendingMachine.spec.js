5f36750b1f56421c72342d68c3d949d37f3f738e
Melanie DeVries <melaniedevries@Melanies-MacBook-Pro.local>
2017-02-06 14:23:29 -0600

    Refactored tests

--- test/vendingMachine.spec.js
+++ test/vendingMachine.spec.js
@@ -1,26 +1,26 @@
 describe("accepts only valid coins", function(){
 
+    var vendingMachine;
+    beforeEach(function(){
+        vendingMachine = new VendingMachine();
+    });
     it("accepts quarters returns true", function(){
         var coin = "quarter";
-        var vendingMachine = new VendingMachine();
         expect(vendingMachine.acceptCoin(coin)).toBe(true);
     });
     it("does not accept pennies returns false", function()
     {
         var coin = "penny";
-        var vendingMachine = new VendingMachine();
         expect(vendingMachine.acceptCoin(coin)).toBe(false);
     });
     it("accepts nickels return true", function()
     {
         var coin = "nickel";
-        var vendingMachine = new VendingMachine();
         expect(vendingMachine.acceptCoin(coin)).toBe(true);
     });
     it("accepts dimes returns true", function(){
 
         var coin = "dime";
-        var vendingMachine = new VendingMachine();
         expect(vendingMachine.acceptCoin(coin)).toBe(true);
     });
 
